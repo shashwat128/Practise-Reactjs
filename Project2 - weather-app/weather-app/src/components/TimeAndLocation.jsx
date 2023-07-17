d79ad7fbd621c046b8c0d87acdecb5c5e107ac5c
Shashwat Singh <shashwat2048@gmail.com>
2023-07-17 17:52:51 +0530

    Time, date and Location

---
+++ Project2 - weather-app/weather-app/src/components/TimeAndLocation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-5">
+        <p className="text-white text-xl font-extralight">
+          Monday, 16 July 2023 | Local time: 12:46
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Mumbai, IN</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
