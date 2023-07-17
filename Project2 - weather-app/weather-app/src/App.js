be3545b6d2149b99fdffe1e3819eac4b161c2e06
Shashwat Singh <shashwat2048@gmail.com>
2023-07-17 13:15:28 +0530

    Project 2 - Weather app, 1. Header - name of cities.

---
+++ Project2 - weather-app/weather-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./components/TopButtons";
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+    </div>
+  );
+}
+
+export default App;
---
+++ Project2 - weather-app/weather-app/src/components/TopButtons.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function TopButtons() {
+  const cities = [
+    {
+      id: 1,
+      title: "Mumbai",
+    },
+    {
+      id: 2,
+      title: "Tokyo",
+    },
+    {
+      id: 3,
+      title: "Glasgow",
+    },
+    {
+      id: 4,
+      title: "London",
+    },
+    {
+      id: 5,
+      title: "Paris",
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-around my-5">
+      {cities.map((city) => (
+        <button key={city.id} className="text-white text-lg font-medium">
+          {city.id}: {city.title}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons;
