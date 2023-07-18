bfc2c66618d96a6b7cb6a215ede1c76e63a3dbfb
Shashwat Singh <shashwat2048@gmail.com>
2023-07-18 18:41:13 +0530

    searching city through search bar

--- Project2 - weather-app/weather-app/src/components/Inputs.jsx
+++ Project2 - weather-app/weather-app/src/components/Inputs.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
-function Inputs() {
+function Inputs({ setQuery, units, setUnits }) {
+  const [city, setCity] = useState("");
+
+  const handleSearchClick = () => {
+    if (city !== "") setQuery({ q: city });
+  };
+
   return (
     <div className="flex flex-row justify-center my-5">
       <div className="flex flex-row w-3/4 items-center space-x-4">
         <input
+          value={city}
+          onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           placeholder="search the city.."
           className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
@@ -13,6 +21,7 @@ function Inputs() {
         <UilSearch
           size={35}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleSearchClick}
         />
         <UilLocationPoint
           size={35}
