b151257599c99f55e4a281c482de58b85fcbb1f4
Shashwat Singh <shashwat2048@gmail.com>
2023-07-18 18:34:04 +0530

    Changing cities with real time data using top buttons

--- Project2 - weather-app/weather-app/src/components/TopButtons.jsx
+++ Project2 - weather-app/weather-app/src/components/TopButtons.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function TopButtons() {
+function TopButtons({ setQuery }) {
   const cities = [
     {
       id: 1,
@@ -27,8 +27,12 @@ function TopButtons() {
   return (
     <div className="flex items-center justify-around my-5">
       {cities.map((city) => (
-        <button key={city.id} className="text-white text-lg font-medium">
-          {city.id}: {city.title}
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
         </button>
       ))}
     </div>
